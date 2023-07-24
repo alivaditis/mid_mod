74fb0cce46bd3172b9271d1d0c94508cf3ea44c9
Alec Livaditis <aleclivaditis@gmail.com>
2023-07-24 12:59:22 -0400

    add test landing on page and userflow of submitting new trick

---
+++ cypress/e2e/app.cy.js
@@ -0,0 +1,28 @@
+describe('App Test', () => {
+  beforeEach(() => {
+    cy.visit('localhost:3000')
+    cy.intercept('GET', 'http://localhost:3001/api/v1/tricks', {fixture: 'tricks.json'}).as('getTricks')
+  })
+  
+  it('As a user I should see a list of tricks, fill out the form, click the send button, and see the updated tricks with the appropriate information', () => {
+    cy.wait('@getTricks')
+      .get('.tricks')
+      .find('.card').should('have.length', 3)
+      .get('.card').first().contains("p", "regular treflip")
+      .get('.card').first().contains("p", "flat ground")
+      .get('.card').first().contains("a", "https://www.youtube.com/watch?v=XGw3YkQmNig")
+      .get('.form--stance').select('regular')
+      .get('.form--name').type('kickflip')
+      .get('.form--obstacle').select('ledge')
+      .get('.form--tutorial').type('https://www.youtube.com/shorts/7MqNsrwZVZE')
+      .get('.form--send').click()
+      .get('.tricks')
+      .find('.card').should('have.length', 4)
+      .get('.card').first().contains("p", "regular treflip")
+      .get('.card').first().contains("p", "flat ground")
+      .get('.card').first().contains("a", "https://www.youtube.com/watch?v=XGw3YkQmNig")
+      .get('.card').last().contains("p", "regular kickflip")
+      .get('.card').last().contains("p", "ledge")
+      .get('.card').last().contains("a", "https://www.youtube.com/shorts/7MqNsrwZVZE")
+  })
+})
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -16,16 +16,16 @@ function Form({addTrick}) {
     <form className='form--tricks'>
       <select className='form--stance' value={stance} onChange={event => setStance(event.target.value)} >
         <option value="" selected disabled hidden>Choose your Stance</option>
-        <option value='Regular'>Regular</option>
-        <option value='Switch'>Switch</option>
+        <option value='regular'>Regular</option>
+        <option value='switch'>Switch</option>
       </select>
       <input className='form--name' name='name' type='text' placeholder='Name of Trick' value={name} onChange={event => setName(event.target.value)}/>
       <select className='form--obstacle' value={obstacle} onChange={event => setObstacle(event.target.value)}>
         <option value="" selected disabled hidden>Choose your Obstacle</option>
-        <option value='Flatground'>Flatground</option>
-        <option value='Ledge'>Ledge</option>
-        <option value='Rail'>Rail</option>
-        <option value='Stairs'>Stairs</option>
+        <option value='flat ground'>Flatground</option>
+        <option value='ledge'>Ledge</option>
+        <option value='rail'>Rail</option>
+        <option value='stairs'>Stairs</option>
       </select>
       <input className='form--tutorial' name='tutorial' placeholder='Tutorial Link' type='text' value={tutorial} onChange={event => setTutorial(event.target.value)}/>
       <button className='form--send' type='submit' onClick={handleClick}>Send It!</button>
